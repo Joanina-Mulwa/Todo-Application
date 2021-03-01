91513df841361aaa641ca9dbb56dc76e0396207d
Joanina-Mulwa <joaninahmulwa@gmail.com>
2021-03-01 16:53:05 +0300

    First Commit

---
+++ todo-api/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+//crub
+//List items
+//GET /todos
+
+//list one item
+//GET /todos/:id
+
+//Creating a new item to db
+//POST /todos
+
+//Editing a item
+//PUT /todos/:id
+
+//Deleting an item from db
+//delete /todos/:id

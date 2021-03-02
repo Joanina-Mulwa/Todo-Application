74e3c6ac99d3eadf6badffcc9326957c35318893
Joanina-Mulwa <joaninahmulwa@gmail.com>
2021-03-02 11:09:23 +0300

    List all items

--- todo-api/index.js
+++ todo-api/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-//crub
+//CRUD REST API
 //List items
 //GET /todos
 
@@ -16,3 +16,32 @@ const app = express();
 
 //Deleting an item from db
 //delete /todos/:id
+
+let todos= [
+    {
+        id : 1,
+        name : "Brush Teeth"
+    },
+
+    {
+        id : 2,
+        name : "Take a shower"
+    },
+
+    {
+        id : 3,
+        name : "Watch a movie"
+    }
+]
+
+app.get('/todos' ,function (req, res) {
+
+    console.log("my todos", todos);
+    return res.send(todos)
+
+})
+
+app.listen(8000 , function () {
+    console.log("Crud Rest API");
+})
+
